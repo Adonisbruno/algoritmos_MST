52aed1b3b9209efdff3b78d60183c4036f46df7f
AdonisBruno <bruno.adonis.sa@gmail.com>
2017-10-02 15:57:17 -0300

    ajustes-comentarios

--- auxiliar.py
+++ auxiliar.py
@@ -1,24 +1,34 @@
 import numpy as np
 
 def lerArquivoToList(file, flag):
-    # usando no DIJKSTRA e PRIM
+    # Funcao ler arquivo linha a linha e converte em uma lista
+    # file := local e nome do arquivo ".../dij10.txt"
+    #  flag := modo de leitura "r"
+
+    # array de retorno
     arr = []
     inp = open(file, flag)
     # ler a linha do arquivo
     for line in inp.readlines():
         # adiciona uma sublista a lista
         arr.append([])
-        # separa os elementos nos espacos, a cada espaco, separa...
+        # separa os elementos nos espacos
         for i in line.split():
-            # converte para inteiro e adiciona ao final da linha
+            # converte valor para inteiro e adiciona ao final
             arr[-1].append(int(i))
     inp.close()
     return arr
 
 
 def loadMatrizAdjacencia(inputList, ordem):
+    # inputList := eh a lista de saida da funcao "lerArquivoToList"
+    # ordem := primeiro elemento da lista de saida de "lerArquivoToList" (dimensao da matrix)
+    
+    # array de saida
     m = np.zeros((ordem, ordem), dtype=int)
     cc = 1
+    # varre a lista de entrada copiando os elementos
+    # para triangulo superior e inferior da matriz de respecitva ordem
     for i, c in enumerate(inputList):
         for j in range(len(inputList[i])):
             # print(i-1,j+cc-1, arr[i][j])
--- mainPRIM-DIJKSTRA.py
+++ mainPRIM-DIJKSTRA.py
@@ -2,12 +2,14 @@ from auxiliar import lerArquivoToList, loadMatrizAdjacencia
 import numpy as np
 import sys
 
+# foi criada uma classe Grafo para facilitar a implementação do PRIM e DIJKSTRA
 class Grafo():
+    # função de inicialização do grafo
     def __init__(self, vertices):
         self.V = vertices
         self.grafo = np.zeros((vertices, vertices), dtype=np.int)
 
-    # Uma funcao de utilidade para imprimir o MST construido armazenado no pai []
+    # Uma funcao para imprimir o MST_PRIM construido armazenado no pred[]
     def printMST_PRIM(self, pred):
         custo = 0
         print("(v[i]-pred[i])  Custo")
@@ -16,12 +18,13 @@ class Grafo():
             custo += self.grafo[i][pred[i]]
         print("\n\nCusto Total: ", custo)
 
+    # funcao para imprimir a saida do DIJKSTRA
     def printDIJKSTRA(self, pred, d):
         print("(v[i]-pred[i])  Distancia da Origem")
         for i in range(1, self.V):
             print(i, "-", pred[i], "\t\t", d[i])
 
-    # Funcao de utilidade para encontrar o vertice com valor de distancia minima
+    # Funcao para encontrar o vertice com valor de distancia minima
     def EXTRACT_MIN(self, chave, mstSet):
         # Inicializa minimo com infinito
         min_index = 0
@@ -32,10 +35,9 @@ class Grafo():
                 min_index = v
         return min_index
 
-    # Funcao para construir e imprimir MST para um grafico representado usando
+    # Funcao para construir MST para um grafico representado usando
     # representacao da matriz de adjacencia
     def MST_PRIM(self):
-        # Primeiro nodo sera a raiz sempre
 
         # Chave utilizados para escolher a ponta minima em corte
         chave = [np.inf] * self.V
@@ -45,11 +47,10 @@ class Grafo():
         pred = [None] * self.V
         # Faca a chave 0 para que este vertice seja escolhido como primeiro vertice
         chave[0] = 0
-        # raiz
+        # Primeiro nodo sera a raiz sempre
         pred[0] = -1
 
         for cout in range(self.V):
-            # while len(Q) > 0:
             # Escolha o vertice de distancia minima do conjunto de vertices nao ainda processado.
             u = self.EXTRACT_MIN(chave, mstSet)
             # Coloque o vertice da distancia minima na arvore
@@ -64,6 +65,7 @@ class Grafo():
         return pred
 
     def DIJKSTRA(self):
+
         # distancias
         d = [np.inf] * self.V
         # Array para armazenar MST
@@ -80,7 +82,7 @@ class Grafo():
             mstSet[u] = True
 
             for v in range(self.V):
-                # relaxxamento
+                # relaxamento do DIJKSTRA
                 if self.grafo[u][v] > 0 and mstSet[v] == False and d[v] > d[u] + self.grafo[u][v]:
                     d[v] = d[u] + self.grafo[u][v]
                     pred[v] = u
@@ -89,21 +91,24 @@ class Grafo():
 def main():
     global vetor, opt
     if len(sys.argv) < 3:
-        print "python mainPRIM-DIJKSTRA.py instancias_Dijkstra_e_PRIM/dij10.txt PRIM | DIJK > saidaPD.txt"
-        print "Exemplo: python mainPRIM-DIJKSTRA.py instancias_Dijkstra_e_PRIM/dij10.txt DIJK > saidaPD.txt"
+        print("python mainPRIM-DIJKSTRA.py instancias_Dijkstra_e_PRIM/dij10.txt PRIM | DIJK > saidaPD.txt")
+        print("Exemplo: python mainPRIM-DIJKSTRA.py instancias_Dijkstra_e_PRIM/dij10.txt DIJK > saidaPD.txt")
         sys.exit(-1)
     try:
+        # ler arquivo para uma lista
         vetor = lerArquivoToList(sys.argv[1], "r")
         opt = sys.argv[2]
     except ValueError:
-        print "valores invalidos"
+        print("valores invalidos")
         sys.exit(-1)
 
 
 if __name__ == "__main__":
     main()
 
+    # retira o primeiro elemento que é a ordem da matriz
     ordem = vetor[0].pop()
+    # com os demais elementos carrega na matriz de adjacencias
     matrixAdj = loadMatrizAdjacencia(vetor, ordem)
 
     if opt == 'PRIM':
